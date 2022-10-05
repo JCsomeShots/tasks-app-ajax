193cf0bd3de87770e6efcce9b1b1b1c53f559eee
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-10-05 12:26:16 +0200

    Form Search implement

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 
 $(function(){
-    console.log('Jquery is working');
+    // console.log('Jquery is working');
+    $('#task-result').hide();
 
-})
+    $('#search').keyup(function(e){
+        if($('#search').val()){
+            let search = $('#search').val();
+            // console.log(sear ch);
+            
+            $.ajax({
+                url : 'task-search.php',
+                type: 'POST',
+                data: {search},
+                success: function(response){
+                    // console.log(response);
+                    let tasks = JSON.parse(response);
+                    // console.log(tasks);
+                    let template = '';
+                    tasks.forEach(task => {
+                        // console.log(task)
+                        template += `<li>${task.name}</li>`
+                    });
+                    $('#container').html(template);
+                    $('#task-result').show();
+
+                }
+            });
+        }
+    })
+
+}) 
--- database.php
+++ database.php
@@ -1,15 +1,29 @@
 <?php
 
-    $connection = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'tasks-app'
-    );
+    // $connection = mysqli_connect(
+    //     'localhost',
+    //     'root',
+    //     'password',
+    //     'tasks-app'
+    // );
 
-    if($connection){
-        echo 'Database is connected';
+    // if($connection){
+    //     echo 'Database is connected';
+    // }
+    $servername = "localhost";
+    $database = "tasks-app";
+    $username = "root";
+    $password = "root";
+    // Create connection
+    $connection = mysqli_connect($servername, $username, $password, $database);
+    // Check connection
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
     }
+    // echo "Connected successfully";
+    // mysqli_close($connection);
+
 
 
 ?>
+
